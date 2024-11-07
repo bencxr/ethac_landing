85552fe2fefdd6ee268f8188bf6cc7541101a4c0
Benedict Chan <bencxr@fragnetics.com>
2024-11-07 14:46:40 +0800

    remove incorrect import

--- backend/aitest.js
+++ backend/aitest.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import OpenAI from "openai";
 const openai = new OpenAI({ apiKey: process.env.OPI_KEY });
 import axios from "axios";
-import extractor from 'unfluff';
 
 async function fetchHTML(url) {
     try {
